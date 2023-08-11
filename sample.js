4f0cdd76df2b799076dfb98c8f32e27cd413568c
rethenya <105271862+Rethenya-C-R@users.noreply.github.com>
2023-08-11 17:02:17 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,16 @@
+
+window.alert("About to witness the most creative page")
+
+function p1fun(){
+    document.getElementById("Hi").innerHTML ="HI! This is Rethenya C R"
+}
+function p2fun(){
+    document.getElementById("Hi").innerHTML ="B-Tech AI&DS"
+}
+function p3fun(){
+    document.getElementById("Hi").innerHTML ="Please don't"
+}
+function p4fun(){
+    document.getElementById("Hi").innerHTML ="Learning full stack development"
+}
+
